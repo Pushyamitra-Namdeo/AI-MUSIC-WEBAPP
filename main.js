f9a83fbfc0eddb90fcf36c1ef816c111246ff02f
Pushyamitra-Namdeo <79986813+Pushyamitra-Namdeo@users.noreply.github.com>
2022-02-09 18:02:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 song1="";
 song2="";
+
+leftWristx=0;
+leftWristy=0;
+
+rightWristx=0;
+rightWristy=0;
+
 function preload(){
 song1= loadSound("music.mp3");
 song2= loadSound("music2.mp3");
+
 }
 
 function setup(){
@@ -10,11 +18,32 @@ canvas= createCanvas(600,500);
 canvas.center();
 video= createCapture(VIDEO);
 video.hide();
+
+posenet= ml5.poseNet(video,model_loaded);
+posenet.on('pose', got_poses);
 }
 
 function draw(){
     image(video,0,0,600,500);
 }
+
+function model_loaded(){
+    console.log("Model is Loaded");
+}
+
+function got_poses(results){
+    if(results.length>0){
+    
+    console.log(results);
+    leftWristx= results[0].pose.leftWrist.x;
+    leftWristy= results[0].pose.leftWrist.y;
+    
+    rightWristx= results[0].pose.rightWrist.x;
+    rightWristy= results[0].pose.rightWrist.y;
+    console.log("leftWristx = "+ leftWristx+"leftWrisrty"+leftWristy);
+    console.log("rightWristx = "+ rightWristx+"rightWrisrty"+rightWristy)
+    }
+    }
 
 
 
