b9d6f9b0d906730d1ee92a5dc8fe936dff88003e
Pushyamitra-Namdeo <79986813+Pushyamitra-Namdeo@users.noreply.github.com>
2022-03-21 18:17:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ leftWristy=0;
 rightWristx=0;
 rightWristy=0;
 
+scoreRight=0;
+scoreLeft=0;
+
 function preload(){
 song1= loadSound("music.mp3");
 song2= loadSound("music2.mp3");
@@ -25,6 +28,22 @@ posenet.on('pose', got_poses);
 
 function draw(){
     image(video,0,0,600,500);
+     fill("#FF0000")
+    if(scoreRight > 0.2){
+        song2.stop();
+        song1.play();
+
+        circle(rightWristx,rightWristy,20);
+    document.getElementById("songName").innerHTML= "SONG 1"
+    }
+    if(scoreLeft > 0.2){
+        song2.play();
+        song1.stop();
+
+        circle(leftWristx,leftWristy,20);
+        document.getElementById("songName").innerHTML= "SONG 2"
+
+    }
 }
 
 function model_loaded(){
@@ -37,7 +56,10 @@ function got_poses(results){
     console.log(results);
     leftWristx= results[0].pose.leftWrist.x;
     leftWristy= results[0].pose.leftWrist.y;
-    
+    scoreLeft= results[0].pose.keypoints[9].score;
+    scoreRight= results[0].pose.keypoints[10].score;  
+    console.log("SL" + scoreLeft);
+    console.log("SR" + scoreRight);  
     rightWristx= results[0].pose.rightWrist.x;
     rightWristy= results[0].pose.rightWrist.y;
     console.log("leftWristx = "+ leftWristx+"leftWrisrty"+leftWristy);
